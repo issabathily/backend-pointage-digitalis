bf98731c2e1f7603372bab38ac3e62211f26a7cf
issa <issabathily388@gmail.com>
2026-04-29 16:45:56 +0000

    v2-fonctionel

--- BackendPointage/settings.py
+++ BackendPointage/settings.py
@@ -211,15 +211,15 @@ ALLOWED_HOSTS = [
 
 # ✅ CORS (autoriser ton frontend)
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    # "http://localhost:5173",
+    # "http://127.0.0.1:5173",
     "https://pointage-digitalis-sn.onrender.com",
 ]
 
 # ✅ CSRF (important pour POST/login)
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    # "http://localhost:5173",
+    # "http://127.0.0.1:5173",
     "https://pointage-digitalis-sn.onrender.com",
 ]
 
